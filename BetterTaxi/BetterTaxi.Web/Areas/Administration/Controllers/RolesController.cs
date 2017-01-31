e0c35e45749eda9545e12bea2685e9f0ec73482c
ARIF MONDAL <ARIF MONDAL>
2017-01-31 15:24:29 +0300

    Admin and Management areas added

---
+++ BetterTaxi/BetterTaxi.Web/Areas/Administration/Controllers/RolesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BetterTaxi.Web.Areas.Administration.Controllers
+{
+    public static class MyExtensions
+    {
+        public static SelectList ToSelectList<TEnum>(this TEnum enumObj)
+            where TEnum : struct, IComparable, IFormattable, IConvertible
+        {
+            var values = from TEnum e in Enum.GetValues(typeof(TEnum))
+                         select new { Id = e, Name = e.ToString() };
+            return new SelectList(values, "Id", "Name", enumObj);
+        }
+    }
+    public class RolesController : Controller
+    {
+        // GET: Administration/Roles
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BetterTaxi/BetterTaxi.Web/Areas/Management/Controllers/TaxiesController.cs
@@ -0,0 +1,17 @@
+﻿using BetterTaxi.Models;
+using BetterTaxi.Web.Infrastructure.Authorization;
+using System;
+using System.Web.Mvc;
+
+namespace BetterTaxi.Web.Areas.Management.Controllers
+{
+    [AuthorizeRoles(UserRole = UserRoles.Administrator, SecondRole = UserRoles.Manager)]
+    public class TaxiesController : Controller
+    {
+        // GET: Management/Taxies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
